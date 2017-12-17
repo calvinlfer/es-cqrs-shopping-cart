2d92f53ee2705ddb234c030ae7c59889fe062a67
Calvin L. Fernandes <calvin.l.fer@outlook.com>
2017-12-17 17:54:49 -0500

    Remove file logger

--- shopping-cart-query-vendor-billing/src/main/resources/logback.groovy
+++ shopping-cart-query-vendor-billing/src/main/resources/logback.groovy
@@ -7,16 +7,8 @@ appender(name="CONSOLE", clazz=ConsoleAppender) {
     }
 }
 
-appender(name="FILE", FileAppender) {
-    file = "query.log"
-    append = true
-    encoder(PatternLayoutEncoder) {
-        pattern = "%date{ISO8601} [%level] %logger %X{akkaSource} message=%msg\n"
-    }
-}
-
 logger(name="akka", level=DEBUG)
 logger(name="org.apache.zookeeper", level=WARN)
 logger(name="akka.persistence.cassandra.query.EventsByTagStage", level=INFO)
 
-root(level=INFO, appenderNames=["CONSOLE", "FILE"])
+root(level=INFO, appenderNames=["CONSOLE"])
